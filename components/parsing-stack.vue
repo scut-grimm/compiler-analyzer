8fdb8528c504dd8c9ac25a9f57c3a3a180a70cdb
AsahiMoon <814256112@qq.com>
2019-05-20 18:16:33 +0800

    add:分析栈展示

---
+++ components/parsing-stack.vue
@@ -0,0 +1,260 @@
+<template>
+  <div class="analysis">
+    <div class="left">
+      <userInput
+        :grammar = grammar
+        @getInput="getData">
+      </userInput>
+      <el-input
+        type="textarea"
+        :rows="12"
+        placeholder="预测分析表"
+      >
+      </el-input>
+      <div style="position: relative; bottom: 10px;left: 10px;">
+        <template v-if="started === false">
+          <el-button type="primary" @click="start">开始</el-button>
+        </template>
+        <template v-if="started === true && isAllDone === false">
+          <el-button type="success" @click="next">下一步</el-button>
+          <el-button type="warning" @click="skip">跳过</el-button>
+          <el-button type="info" @click="startAutoPlay" v-if="autoTimer === null">自动播放</el-button>
+          <el-button type="danger" @click="stopAutoPlay" v-if="autoTimer !== null">停止播放</el-button>
+        </template>
+        <el-button @click="restart" v-if="started" type="primary">重新开始</el-button>
+      </div>
+    </div>
+
+    <div class="right">
+      <el-table
+        :data="stackData"
+        style="width: 100%">
+        <el-table-column
+          prop="matched"
+          label="已匹配"
+          width="180"
+          align="right">
+        </el-table-column>
+        <el-table-column
+          prop="symbolStack"
+          label="符号栈"
+          width="180"
+          align="right">
+        </el-table-column>
+        <el-table-column
+          prop="input"
+          label="输入"
+          align="right">
+        </el-table-column>
+        <el-table-column
+          prop="action"
+          label="动作"
+          align="right">
+        </el-table-column>
+      </el-table>
+
+  </div>
+  </div>
+</template>
+<script>
+  import userInput from '~/components/userInput'
+  import AlgorithmWrapper from '~/classes/algorithm-wrapper'
+  import PredictiveParsingTable from '~/classes/predictive-parsing-table'
+  import GeneratePredictiveParsingTable from '~/classes/algorithms/generate-predictive-parsing-table'
+  import Grammar from '~/classes/grammar'
+  import MapSet from '~/classes/map-set'
+  import ParsingStack from '~/classes/parsing-stack'
+  import GenerateParsingStack from '~/classes/algorithms/generate-parsing-stack'
+  export default {
+    components:{
+      userInput
+    },
+    data(){
+      const parsingStack = new ParsingStack()
+      return {
+        grammar: new Grammar(),
+        active: 0,
+        parsingStack,
+        stack:this.parsingStack,
+        PPT:'',
+        PPTData:'',
+        strToken:'',
+        inputData:'',
+        Production:'',
+        stackData:[],
+        wrapper: null,
+        pre_notice: '',
+        notice: '',
+        started: false,
+        curStep: -1,
+        autoTime: 1000,
+        autoTimer: null,
+        isAllDone:false
+      }
+    },
+    methods:{
+      getData(val){
+        if(val !== '')
+        this.inputData = val
+      },
+      start(){
+        const algorithm = new GenerateParsingStack(this.grammar,this.PPT,this.inputData)
+        this.wrapper = new AlgorithmWrapper(algorithm)
+        this.wrapper.init()
+        this.started = true
+        this.isAllDone = false
+        this.next()
+      },
+      restart(){
+        this.stackData = []
+        this.start()
+      },
+      sync() {
+          let Result = this.wrapper.getCurResult()
+          this.stack = Result.stack
+          this.strToken = Result.strToken
+          this.isAllDone = this.wrapper.isAllDone()
+          if (this.isAllDone && this.autoTimer !== null) {
+            clearTimeout(this.autoTimer)
+            this.autoTimer = null
+          }
+          if(this.isAllDone && this.strToken.length>1){
+            alert("无法继续匹配")
+          }
+
+      // todo:测试用，待改
+        this.stackData.push({
+          matched: this.Production,
+          symbolStack: this.stack.getStringStack(),
+          input: this.strTokenData,
+          action: this.notice
+        })
+      },
+      next(){
+        let {Production, notice} = this.wrapper.next()
+        // this.pre_notice = this.notice
+        this.notice = notice
+        this.Production = Production
+        this.sync()
+      },
+      startAutoPlay(){
+        this.autoTimer = setTimeout(() => {this.autoloop()}, this.autoTime)
+      },
+      stopAutoPlay(){
+        clearTimeout(this.autoTimer)
+        this.autoTimer = null
+      },
+      autoloop(){
+        this.next()
+        if(this.isAllDone){
+          this.autoTimer = null
+          return
+        }
+        this.autoTimer = setTimeout(() => {this.autoloop()}, this.autoTime)
+      },
+      skip(){
+        while(!this.isAllDone){
+          this.next()
+        }
+      }
+    },
+    computed:{
+      strTokenData(){
+        let result=''
+        for(let i of this.strToken){
+            result += i.getString()
+        }
+        return result
+      }
+    },
+    mounted() {
+      const grammar = this.grammar
+      const E = grammar.getSign('E', 'Nonterminal')
+      const E1 = grammar.getSign('E\'', 'Nonterminal')
+      const T = grammar.getSign('T', 'Nonterminal')
+      const T1 = grammar.getSign('T\'', 'Nonterminal')
+      const F = grammar.getSign('F', 'Nonterminal')
+      const Plus = grammar.getSign('+', 'Terminal')
+      const Multi = grammar.getSign('*', 'Terminal')
+      const Id = grammar.getSign('id', 'Terminal')
+      const LeftClose = grammar.getSign('(', 'Terminal')
+      const RightClose = grammar.getSign(')', 'Terminal')
+      const Empty = grammar.getSign('ε', 'Terminal')
+      const End = grammar.getSign('$', 'Terminal')
+      grammar.addProduction(E, [T, E1])
+      grammar.addProduction(E1, [Plus, T, E1])
+      grammar.addProduction(E1, [Empty])
+      grammar.addProduction(T, [F, T1])
+      grammar.addProduction(T1, [Multi, F, T1])
+      grammar.addProduction(T1, [Empty])
+      grammar.addProduction(F, [LeftClose, E, RightClose])
+      grammar.addProduction(F, [Id])
+
+      const firstSet = new MapSet()
+      const followSet = new MapSet()
+      firstSet.add(E, LeftClose)
+      firstSet.add(E, Id)
+
+      firstSet.add(T, LeftClose)
+      firstSet.add(T, Id)
+
+      firstSet.add(F, LeftClose)
+      firstSet.add(F, Id)
+
+      firstSet.add(E1, Plus)
+      firstSet.add(E1, Empty)
+
+      firstSet.add(T1, Multi)
+      firstSet.add(T1, Empty)
+
+      followSet.add(E, RightClose)
+      followSet.add(E, End)
+
+      followSet.add(E1, RightClose)
+      followSet.add(E1, End )
+
+      followSet.add(T, Plus)
+      followSet.add(T, RightClose)
+      followSet.add(T, End)
+
+      followSet.add(T1, Plus)
+      followSet.add(T1, RightClose)
+      followSet.add(T1, End)
+
+      followSet.add(F, Plus)
+      followSet.add(F, Multi)
+      followSet.add(F, RightClose)
+      followSet.add(F, End)
+
+
+      grammar.firstSet = firstSet
+      grammar.followSet = followSet
+
+      // console.log("------------------------------------------------")
+      this.PPT = new GeneratePredictiveParsingTable(grammar).run()
+      this.strToken= [grammar.getSign('id', 'Terminal'),grammar.getSign('*', 'Terminal'),grammar.getSign('id', 'Terminal')]
+      this.inputData = [grammar.getSign('id', 'Terminal'),grammar.getSign('*', 'Terminal'),grammar.getSign('id', 'Terminal')]
+      console.log(this.PPT.getTableData())
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .analysis{
+    display: flex;
+    width: 70%;
+    margin: 30px auto;
+    .left{
+      width: 30%;
+      *{
+        margin-bottom: 30px;
+      }
+
+    }
+    .right{
+      /*padding-left: 20px;*/
+      width: 70%;
+      position: relative;
+    }
+  }
+</style>
