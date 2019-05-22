5d16971fc95f9b9c3e80e5ef83f4668a899d695b
AsahiMoon <814256112@qq.com>
2019-05-22 14:39:09 +0800

    add:暂时使用的输入模块

---
+++ components/userInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+  <el-input
+    v-model="input"
+    type="textarea"
+    :rows="12"
+    placeholder="请输入要分析的语句,空格键隔开每个Sign"
+    >
+  </el-input>
+  </div>
+</template>
+
+<script>
+  import grammar from '~/classes/grammar'
+    export default {
+      props: ['grammar'],
+      name: "userInput",
+      data(){
+          return{
+            input:''
+          }
+      },
+      watch:{
+          input(val){
+            const grammar = this.grammar
+            let result = []
+            const arr = val.split(' ')
+            for(let i of arr){
+              result.push(grammar.getSign(i, 'Terminal'))
+            }
+            this.$emit('getInput', result)
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
