b66a423385c4c412b7a286ec85f738beac6fd819
hespoon <zaojianhe@163.com>
2019-06-11 18:56:59 +0800

    消除左递归完成

---
+++ pages/eliminate-left-recursion.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ELR></ELR>
+  </div>
+</template>
+<script>
+import ELR from "~/components/eliminate-left-recursion";
+export default {
+  layout: "grammar",
+  components: {
+    ELR
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
