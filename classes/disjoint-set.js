60293bb57ba35438474e00eb7cf7de7177ef780f
hespoon <zaojianhe@163.com>
2019-06-12 09:03:55 +0800

    格式化disjoint-set.js

--- classes/disjoint-set.js
+++ classes/disjoint-set.js
@@ -1,66 +1,65 @@
 import assert from 'assert'
-class DisjointSet{
-  constructor(){
+class DisjointSet {
+  constructor() {
     this.cnt = 0
     this.map = new Map()
   }
-  add(obj){
-    if(!this.map.has(obj)){
+  add(obj) {
+    if (!this.map.has(obj)) {
       this.map.set(obj, this.cnt++)
     }
     return this.map.get(obj)
   }
-  get(obj){
+  get(obj) {
     assert.strictEqual(this.map.has(obj), true, 'should add before')
     return this.map.get(obj)
   }
-  changeId(oldId, newId){
-    for(let key of this.map.keys()){
-      if(this.map.get(key) === oldId){
+  changeId(oldId, newId) {
+    for (const key of this.map.keys()) {
+      if (this.map.get(key) === oldId) {
         this.map.set(key, newId)
       }
     }
   }
-  reSetId(){
-    let oldIds = new Set([...this.map.keys()].map(e => this.map.get(e)))
+  reSetId() {
+    const oldIds = new Set([...this.map.keys()].map(e => this.map.get(e)))
     let tmpCnt = 0
-    for(let oldId of oldIds){
+    for (const oldId of oldIds) {
       this.changeId(oldId, tmpCnt++)
     }
     this.cnt = tmpCnt
   }
-  disjoint(obj1, obj2){
-    if(this.map.has(obj1) && this.map.has(obj2)){
-      let id = this.get(obj1)
-      let toCorrectId = this.get(obj2)
+  disjoint(obj1, obj2) {
+    if (this.map.has(obj1) && this.map.has(obj2)) {
+      const id = this.get(obj1)
+      const toCorrectId = this.get(obj2)
       this.changeId(toCorrectId, id)
       return id
     }
-    if(this.map.has(obj1)){
+    if (this.map.has(obj1)) {
       this.map.set(obj2, this.get(obj1))
       return this.get(obj1)
     }
-    if(this.map.has(obj2)){
+    if (this.map.has(obj2)) {
       this.map.set(obj1, this.get(obj2))
       return this.get(obj2)
     }
     {
-      let id = this.add(obj1)
+      const id = this.add(obj1)
       this.map.set(obj2, id)
       return id
     }
   }
-  del(obj){
+  del(obj) {
     this.map.delete(obj)
   }
-  print(){
-    let tmp = [...this.map.keys()].sort((a,b) => {
+  print() {
+    const tmp = [...this.map.keys()].sort((a, b) => {
       return this.map.get(a) - this.map.get(b)
     })
-    for(let key of tmp){
+    for (const key of tmp) {
       console.log(key.getString(), this.map.get(key))
     }
   }
-
 }
 export default DisjointSet
