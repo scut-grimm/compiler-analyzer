00226090eda4902a64a2a71f7b1991902fccd75e
hespoon <zaojianhe@163.com>
2019-06-30 10:45:36 +0800

    UI优化完成-何博韬

--- classes/algorithms/is-LL1.js
+++ classes/algorithms/is-LL1.js
@@ -7,7 +7,7 @@ class IsLL1 {
     this.grammar = grammar.clone()
   }
 
-  isLL1New() {
+  isLL1() {
     const empty = this.grammar.getEmptySign()
     const nonterminals = this.grammar.getNonterminals()
     assert(nonterminals.length, '文法没有非终止符号')
@@ -78,64 +78,6 @@ class IsLL1 {
     }
   }
 
-  isLL1() {
-    const nonterminals = this.grammar.getNonterminals()
-    assert(nonterminals.length, '文法没有非终止符号')
-    for (const item of nonterminals) {
-      const productions = this.grammar.getDerivations(item)
-      if (productions.length !== 0) {
-        const nonFirstSet = new Set(this.grammar.getSignFirstSet(item)) // 不要使用字符串
-        // assert(nonFirstSet.size, `First(${item.symbol})为空`)
-        const nonFollowSet = new Set(this.grammar.getSignFollowSet(item)) // 不要使用字符串
-        // assert(nonFollowSet.size, `Follow(${item.symbol})为空`)
-        const intersection = this.intersection(nonFirstSet, nonFollowSet)
-        // 不要使用字符串判断是否有 ε 直接用 set.has(Sign)
-        if (!(nonFirstSet.has(this.grammar.getEmptySign()) && intersection.size > 0)) {
-          const bodyFirstSets = productions.map(x => this.grammar.getProductionBodyFirstSet(x))
-          const errorBodyIndex = this.findProductionBodyFirstSetIntersectionNotEmpty(bodyFirstSets)
-          if (errorBodyIndex.length !== 0) {
-            const noticeArr = Array.of()
-            for (const i of errorBodyIndex) {
-              noticeArr.push(`${this.genErrorNotice(productions, i)}相交不为空`)
-            }
-            const errorProductionArray = this.grammar.getDerivations(item)
-            const errorProduction = {
-              error: true,
-              production: errorProductionArray, // 这里放原始的产生式对象，不要放字符串
-              errorBodyIndex: errorBodyIndex, // 相交不为空的产生式在 production 中的下标
-              notice: noticeArr
-            }
-            this.isLL1Label = false
-            this.productions.push(errorProduction)
-          } else {
-            const rightProductionArray = this.grammar.getDerivations(item)
-            const rightProduction = {
-              error: false,
-              production: rightProductionArray,
-              errorBodyIndex: [],
-              notice: ['产生式没问题']
-            }
-            this.productions.push(rightProduction)
-          }
-        } else {
-          const noticeArr = Array.of(`First(${item.symbol}) 中存在 ε 且 First(${item.symbol}) 与 Follow(${item.symbol}) 相交不为空`)
-          const errorProductionArray = this.grammar.getDerivations(item)
-          const errorProduction = {
-            error: true,
-            production: errorProductionArray, // 这里放原始的产生式对象，不要放字符串
-            notice: noticeArr
-          }
-          this.isLL1Label = false
-          this.productions.push(errorProduction)
-        }
-      }
-    }
-    return {
-      isLL1: this.isLL1Label,
-      productions: this.productions
-    }
-  }
-
   // 输入 二维数组
   // 输出 二维数组
   findProductionBodyFirstSetIntersectionNotEmpty(bodyFirstSets) {
@@ -180,14 +122,6 @@ class IsLL1 {
 }
 export default function(grammar) {
   const temp = new IsLL1(grammar)
-  const result = temp.isLL1New()
-  // result.productions.forEach(e => {
-  //   let productionString = e.production[0].getHeadString() + ' -> '
-  //   for (const i of e.production) {
-  //     productionString += i.getBodyString() + ' | '
-  //   }
-  //   productionString = productionString.slice(0, -3)
-  //   e.production = productionString
-  // })
+  const result = temp.isLL1()
   return result
 }
